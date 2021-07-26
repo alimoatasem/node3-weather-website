9d9f9de5673d9e32c4be9a2223fa6d3569b8f303
alimoatasem <alimoatasem2000@yahoo.com>
2021-07-26 15:13:11 +0200

    Added Humidity info.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,9 @@
 const request = require('request')
 
+
+
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=4feb5e2ac890dfd8dc6159cf141c53cd&query=' + latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=4feb5e2ac890dfd8dc6159cf141c53cd&query=' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, {body}) => {
         if (error) {
@@ -9,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error){
             callback('Unable to find location.', undefined)
         } else {
-         callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+         callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. Humidity is ' + body.current.humidity + '%')
         }
     })
 }
